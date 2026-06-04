576edd184edfd6607cd1c847132ea19f76d762f9
JDKR <57557558+jk-dr@users.noreply.github.com>
2026-06-04 23:17:26 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 document.querySelector("#profile-drop img").src="https://github.com/jk-dr/p/blob/main/IMG_4440.png?raw=true"
 document.querySelector("#profile-accordion img").src="https://github.com/jk-dr/p/blob/main/IMG_4440.png?raw=true"
+document.querySelector("#notificationselector_ext")?.closest('li.actions-small-1.status-read')?.remove();
