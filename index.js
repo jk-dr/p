f2b8ab48407178269a8d2c8a2746e74daf9a7fb3
JDKR <57557558+jk-dr@users.noreply.github.com>
2026-06-04 23:03:26 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-document.querySelectorAll("#profile-drop img").forEach(function(e) {e.src="https://github.com/jk-dr/p/blob/main/IMG_4440.png?raw=true"})
-document.querySelectorAll("#profile-accordion img").forEach(function(e) {e.src="https://github.com/jk-dr/p/blob/main/IMG_4440.png?raw=true"})
+document.querySelector("#profile-drop img").src="https://github.com/jk-dr/p/blob/main/IMG_4440.png?raw=true"})
+document.querySelector("#profile-accordion img").src="https://github.com/jk-dr/p/blob/main/IMG_4440.png?raw=true"})
